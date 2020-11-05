5d1eb9df2b99bb8156c4e140b2b64433946aaefe
rainerweston <rainerweston@gmail.com>
2020-11-05 17:14:37 +1300

    fix (boost): + debug stats (#6)
    * chore(setup): added local /builds/ folder to .gitignore
    
    * refactor: reset position of dropship scene to origin
    
    * feat: added debug overlay feature
    
    * fix(debug): added FPS and system memory to debug overlay
    
    * fix(scene): detached boost signal from dropship script

---
+++ src/levels/main.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	var overlay = load("res://scenes/debug_overlay.tscn").instance()
+	
+	overlay.add_stat("Player position", $Player, "position", false)
+	overlay.add_stat("Mouse position", self, "get_global_mouse_position", true)
+	
+	add_child(overlay)
