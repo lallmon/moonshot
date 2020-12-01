7c7921723fb6851bc8d1eaabb138959de2fee2e4
Rainer Weston <rainerweston@gmail.com>
2020-12-02 06:06:44 +1300

    feat(score): added scorecard splash to main level

--- src/scripts/game.gd
+++ src/scripts/game.gd
@@ -5,6 +5,10 @@ var camera = null setget _set_camera, _get_camera
 var gui = null setget _set_gui, _get_gui
 
 var main = null
+var _filename = "game.dat"
+
+var deepest:int
+var longest:int
 
 var debug = true
 
@@ -30,6 +34,48 @@ func _get_gui():
 
 func _ready():
 	self.pause_mode = PAUSE_MODE_PROCESS #make it process when the game is paused
+
+func save():
+		var save_dict = {
+	        "_filename" : _filename,
+			"longest" : longest,
+			"deepest" : deepest,
+	    }
+		return save_dict
+
+# Note: This can be called from anywhere inside the tree. This function is
+# path independent.
+# Go through everything in the persist category and ask them to return a
+# dict of relevant variables.
+func save_game():
+	var save_game = File.new()
+	save_game.open("user://" + _filename, File.WRITE)
+
+	# Call the node's save function.
+	var node_data = save()
+
+	# Store the save dictionary as a new line in the save file.
+	save_game.store_line(to_json(node_data))
+	save_game.close()
+
+# Note: This can be called from anywhere inside the tree. This function
+# is path independent.
+func load_game():
+	var save_game = File.new()
+	if not save_game.file_exists("user://" + _filename):
+		return # Error! We don't have a save to load.
+
+    # Load the file line by line and process that dictionary to restore
+    # the object it represents.
+	save_game.open("user://" + _filename, File.READ)
+	while save_game.get_position() < save_game.get_len():
+		# Get the saved dictionary from the next line in the save file
+		var node_data = parse_json(save_game.get_line())
+		# Now we set the remaining variables.
+		for i in node_data.keys():
+			self.set(i, node_data[i])
+
+	save_game.close()
 
 
 #OS Input code for fullscreening
